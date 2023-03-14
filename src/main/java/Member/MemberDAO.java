f42c20b97cd21847dbc8a823be713816adfa6b8b
whdidgma94 <whdidgma94@naver.com>
2023-03-14 11:43:52 +0900

    .
    .

--- src/main/java/Member/MemberDAO.java
+++ src/main/java/Member/MemberDAO.java
@@ -1,5 +1,57 @@
 package Member;
 
-public class MemberDAO {
+import org.apache.ibatis.session.SqlSession;
+import Util.MybatisConfig;
 
+
+public class MemberDAO {
+	
+	private MemberDAO() {}
+	static private MemberDAO instance = new MemberDAO();
+	static public MemberDAO getInstance() {
+		return instance;
+	}
+	public String userLogin(MemberVO memberVO) {
+		
+		   SqlSession session= MybatisConfig.getInstance().openSession(true);
+		   String userId=session.selectOne("mapper.member.memberLogin", memberVO);
+		   session.close();
+		   return userId;
+	   }
+	   
+	   public String checkValidId(String id) {
+		   SqlSession session= MybatisConfig.getInstance().openSession(true);
+		   String userId=session.selectOne("mapper.member.validId", id);
+		   session.close();
+		   return userId;
+	   }
+	      
+	   public int addUser(MemberVO memberVO) {
+		   SqlSession session= MybatisConfig.getInstance().openSession(true);
+		   int cnt=session.insert("mapper.member.memberInsert", memberVO);
+		   session.close();
+		   return cnt;
+	   }
+	   
+	   public MemberVO getOneUser(String id) {
+		   SqlSession session= MybatisConfig.getInstance().openSession(true);
+		   MemberVO memberVO = session.selectOne("mapper.member.getOneUser",id);
+		   session.close();
+		   return memberVO;
+	   }
+
+	   
+	   public int userUpdate(MemberVO memberVO) {
+		   SqlSession session= MybatisConfig.getInstance().openSession(true);
+		   int cnt= session.update("mapper.member.memberUpdate",memberVO);
+		   session.close();
+		   return cnt;
+	   }
+	   
+	   public int removeUser(String id) {
+		   SqlSession session= MybatisConfig.getInstance().openSession(true);
+		   int cnt=session.insert("mapper.user.userDelete", id);
+		   session.close();
+		   return cnt;
+	   }
 }
