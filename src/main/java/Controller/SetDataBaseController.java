02cdeecde89f7c18bb70eb080e10e9b75d108f4a
yanhjh <123146563+yanhjh@users.noreply.github.com>
2023-03-15 14:57:22 +0900

    ,
    ,

--- src/main/java/Controller/SetDataBaseController.java
+++ src/main/java/Controller/SetDataBaseController.java
@@ -42,13 +42,19 @@ public class SetDataBaseController implements Controller {
 			JSONParser jp2 = new JSONParser();
 			for (int i = 0; i < 10; i++) {
 				JSONObject jb2 = (JSONObject) jp2.parse(movie);
-				JSONArray audits =  (JSONArray) jb2.get("audits");
-				JSONObject watchGradeNm = (JSONObject) audits.get(1);
-				mvo.setDirector((String) jb2.get("directors.peopleNm"));
-				mvo.setActor((String) jb2.get("actors.peopleNm"));
+				JSONArray audits = (JSONArray) jb2.get("audits");
+				JSONObject watchGradeNm = (JSONObject) audits.get(0);
+				mvo.setWatchGradeNm((String) watchGradeNm.get("watchGradeNm"));
+				JSONArray directors = (JSONArray) jb2.get("directors");
+				JSONObject firstDirector = (JSONObject) directors.get(0);
+				mvo.setDirector((String) firstDirector.get("peopleNm"));
+				JSONArray actors = (JSONArray) jb2.get("actors");
+				if (actors != null && actors.size() > 0) {
+					JSONObject firstActor = (JSONObject) actors.get(0);
+					mvo.setActor((String) firstActor.get("peopleNm"));
+				}
 				mvo.setShowTm(Integer.parseInt((String) jb2.get("showTm")));
 				mvo.setShowTypeNm((String) jb2.get("showTypeNm"));
-				mvo.setMovieInfo((String) jb2.get("movieNm"));	
 				System.out.println((String) jb2.get("genres[0]"));
 				mvo.setGenreNm((String) jb2.get("genres[0]"));
 				MovieDAO.getInstance().insertMovie(mvo);
