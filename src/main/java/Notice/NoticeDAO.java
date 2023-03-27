e52785b578f925b213fcb351fed1d28edb60e73d
yanhjh <junyoungha2@naver.com>
2023-03-27 12:30:03 +0900

    ,
    ,

--- src/main/java/Notice/NoticeDAO.java
+++ src/main/java/Notice/NoticeDAO.java
@@ -8,29 +8,34 @@ import Util.MybatisConfig;
 
 public class NoticeDAO {
 
-	private NoticeDAO() {}
+	private NoticeDAO() {
+	}
+
 	static private NoticeDAO instance = new NoticeDAO();
+
 	static public NoticeDAO getInstance() {
 		return instance;
 	}
-	
+
 	public void addNotice(NoticeVO notice) {
 		SqlSession session = MybatisConfig.getInstance().openSession(true);
 		session.insert("mapper.notice.insertNotice", notice);
 		session.close();
 	}
-	public List<NoticeVO> getAllNotice(){
+
+	public List<NoticeVO> getAllNotice() {
 		SqlSession session = MybatisConfig.getInstance().openSession(true);
 		List<NoticeVO> list = session.selectList("mapper.notice.getAllNotice");
 		session.close();
 		return list;
 	}
+
 	public void removeNotice(int noticeNo) {
 		SqlSession session = MybatisConfig.getInstance().openSession(true);
 		session.insert("mapper.notice.deleteNotice", noticeNo);
 		session.close();
 	}
-	
+
 	public void setDummyData() {
 		SqlSession session = MybatisConfig.getInstance().openSession(true);
 		session.insert("mapper.notice.setDummyData");
